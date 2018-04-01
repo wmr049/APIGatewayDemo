579b1932a8ef999e431a1569cd654af85fa41968
Milton Reis <milton.reis@kroton.com.br>
2018-04-01 18:33:52 -0300

    Add JWT

---
+++ APIGateway/Startup.cs
@@ -0,0 +1,68 @@
+﻿namespace APIGateway
+{
+    using CacheManager.Core;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.IdentityModel.Tokens;
+    using Ocelot.DependencyInjection;
+    using Ocelot.Middleware;
+    using System;
+    using System.Text;
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var audienceConfig = Configuration.GetSection("Audience");
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(audienceConfig["Secret"]));
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = audienceConfig["Iss"],
+                ValidateAudience = true,
+                ValidAudience = audienceConfig["Aud"],
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true,
+            };
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = "TestKey";
+            })
+            .AddJwtBearer("TestKey", x =>
+             {
+                 x.RequireHttpsMetadata = false;
+                 x.TokenValidationParameters = tokenValidationParameters;
+             });
+
+            services.AddOcelot(Configuration);
+        }
+
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            await app.UseOcelot();
+        }
+    }
+}
