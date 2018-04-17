367c891c7eb555c66b847d5eb299460c5f205ec3
Milton Reis <milton.reis@kroton.com.br>
2018-04-17 15:48:05 -0300

    ajuste issuer aud

--- AuthServer/Startup.cs
+++ AuthServer/Startup.cs
@@ -20,20 +20,15 @@ namespace AuthServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddOptions();
+            services.Configure<Controllers.Audience>(Configuration.GetSection("Audience"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+        {            
             app.UseMvc();
         }
     }
